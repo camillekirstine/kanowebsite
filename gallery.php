4ceb75745229886e463989d8b8139ceab53ee05d
Charlie <camille@camillekirstine.com>
2023-06-10 18:59:09 +0200

    Started setting up the gallery

--- gallery.php
+++ gallery.php
@@ -35,22 +35,109 @@
     <script src="https://kit.fontawesome.com/f5df81b727.js" crossorigin="anonymous"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <style>
+        main {
+            margin-top: 7rem;
+            height: calc(100vh - 7vh);
+        }
+    </style>
+
 </head>
 
 <body>
 <?php include "includes/nav.php"?>
 <main>
 
-    <div class="row">
-        <section class="col col-lg-6"></section>
+    <section class="m-5 px-5">
+        <div class="row pt-4">
+            <section class="col-12 text-center mb-3">
+                <h1>Gallery</h1>
+                <h2>Here you can see pictures of some of the work we have done.</h2>
+            </section>
 
-        <aside class="col col-lg-6"></aside>
-    </div>
+            <div class="row">
+                <div class="col-12 col-lg-6 p-3">
+                    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="gallery/gallery1.webp" class="d-block w-100" alt="picture of some of the work we do in the salon">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="gallery/gallery2.webp" class="d-block w-100" alt="picture of some of the work we do in the salon">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="gallery/gallery3.webp" class="d-block w-100" alt="picture of some of the work we do in the salon">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="gallery/gallery4.webp" class="d-block w-100" alt="picture of some of the work we do in the salon">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="gallery/gallery6.webp" class="d-block w-100" alt="picture of some of the work we do in the salon">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="gallery/gallery7.webp" class="d-block w-100" alt="picture of some of the work we do in the salon">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="gallery/gallery8.webp" class="d-block w-100" alt="picture of some of the work we do in the salon">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="gallery/gallery8.webp" class="d-block w-100" alt="picture of some of the work we do in the salon">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="gallery/gallery9.webp" class="d-block w-100" alt="picture of some of the work we do in the salon">
+                            </div>
+                        </div>
+
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 pb-lg-2">
+                    <div class="row">
+                        <div class="col-6 col-lg-4 p-3">
+                            <img alt="picture of some of the work we do in the salon" src="gallery/gallery1.webp">
+                        </div>
+                        <div class="col-6 col-lg-4 p-3">
+                            <img alt="picture of some of the work we do in the salon" src="gallery/gallery2.webp">
+                        </div>
+                        <div class="col-6 col-lg-4 p-3">
+                            <img alt="picture of some of the work we do in the salon"  src="gallery/gallery3.webp">
+                        </div>
+                        <div class="col-6 col-lg-4 p-3">
+                            <img alt="picture of some of the work we do in the salon" src="gallery/gallery4.webp">
+                        </div>
+                        <div class="col-6 col-lg-4 p-3">
+                            <img alt="picture of some of the work we do in the salon" src="gallery/gallery5.webp">
+                        </div>
+                        <div class="col-6 col-lg-4 p-3">
+                            <img alt="picture of some of the work we do in the salon" src="gallery/gallery6.webp">
+                        </div>
+                        <div class="col-6 col-lg-4 p-3">
+                            <img alt="picture of some of the work we do in the salon" src="gallery/gallery7.webp">
+                        </div>
+                        <div class="col-6 col-lg-4 p-3">
+                            <img alt="picture of some of the work we do in the salon" src="gallery/gallery8.webp">
+                        </div>
+                        <div class="col-6 col-lg-4 p-3">
+                            <img alt="picture of some of the work we do in the salon" src="gallery/gallery9.webp">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
-
+    <?php include "includes/footer.php"?>
 </main>
-<?php include "includes/footer.php"?>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
